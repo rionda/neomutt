e0b320deb1cda056974f1af0447ae34ec1274270
Richard Russon <rich@flatcap.org>
2016-06-01 21:38:22 +0100

    mutt-1.5.23-ssl_ciphers

--- conn/config.c
+++ conn/config.c
@@ -55,7 +55,8 @@ static struct ConfigDef ConnVarsSsl[] = {
   { "certificate_file", DT_PATH|DT_PATH_FILE, IP "~/.mutt_certificates", 0, NULL,
     "File containing trusted certificates"
   },
-  { "ssl_ciphers", DT_STRING, 0, 0, NULL,
+#endif
+  { "ssl_ciphers", DT_STRING, IP "@SYSTEM", 0, NULL,
     "Ciphers to use when using SSL"
   },
   { "ssl_client_cert", DT_PATH|DT_PATH_FILE, 0, 0, NULL,
